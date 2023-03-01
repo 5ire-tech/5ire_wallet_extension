ffc0f2adee8c36185dfff2a5425064020bdcf010
rahul <rahul.saharan@antiersolutions.com>
2023-03-01 18:05:44 +0530

    conflect fix

--- src/Pages/WelcomeScreens/CreateWalletChain.jsx
+++ src/Pages/WelcomeScreens/CreateWalletChain.jsx
@@ -22,12 +22,6 @@ function CreateWalletChain() {
     let res;
     if (((newAccount === null || !newAccount) && !isLogin) || isLogin)
       res = walletSignUp();
-<<<<<<< Updated upstream
-=======
-    if (isLogin) res = walletSignUp();
-
-    // console.log("res: ", res);
->>>>>>> Stashed changes
 
     if (res?.error) toast.error(res.data);
 
