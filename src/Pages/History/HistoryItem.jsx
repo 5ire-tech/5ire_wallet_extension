364e6888f5fae8b8a17184dbaf626acab176479d
Shweta Saini <shweta@antiersolutions.com>
2023-04-28 15:49:52 +0530

    change in txHistory module

--- src/Pages/History/HistoryItem.jsx
+++ src/Pages/History/HistoryItem.jsx
@@ -3,6 +3,7 @@ import style from "./style.module.scss";
 import Swap from "../../Assets/swap.svg";
 import Sent from "../../Assets/sent.svg";
 import { STATUS } from '../../Constants';
+import { fixNumber } from "../../Helper/helper";
 import { formatDate, shortner } from "../../Helper/helper"
 
 export default function HistoryItem({ historyItem, handleHistoryOpen }) {
@@ -13,14 +14,17 @@ export default function HistoryItem({ historyItem, handleHistoryOpen }) {
       </div>
       <div className={style.historySec__historyMarketSwap}>
         <div className={style.historySec__historyMarketSwap__leftSide}>
-          <img src={historyItem.intermidateHash ? Swap : Sent} />
+          <img alt="hash" src={historyItem.intermidateHash ? Swap : Sent} />
+
           <div className={style.historySec__historyMarketSwap__leftContact} onClick={() => { handleHistoryOpen(historyItem) }}>
             <h3>{historyItem.type}</h3>
             <p>{historyItem.to && historyItem.to.length > 40 ? shortner(historyItem.to) : historyItem.to}</p>
           </div>
+
         </div>
+        
         <div className={style.historySec__historyMarketSwap__rytSide}>
-          <h3>{historyItem.amount} 5ire</h3>
+          <h3>{historyItem?.amount ? fixNumber(historyItem.amount) : ""} 5ire</h3>
           <p>
             Status : <span className={(historyItem.status.toLowerCase() === STATUS.PENDING.toLowerCase() || historyItem.status.toLowerCase() === STATUS.QUEUED.toLowerCase()) ? style.historySec__pending : (historyItem.status.toLowerCase() === STATUS.SUCCESS.toLowerCase() ? style.historySec__success : style.historySec__failed)}>{historyItem.status}</span>
           </p>
