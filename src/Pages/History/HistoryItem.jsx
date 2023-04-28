2430e8a67ca842c52e2bb5d666b727e63057eb5b
Shweta Saini <shweta@antiersolutions.com>
2023-04-28 16:01:13 +0530

    fixed zero value case in toFixed method

--- src/Pages/History/HistoryItem.jsx
+++ src/Pages/History/HistoryItem.jsx
@@ -3,7 +3,7 @@ import style from "./style.module.scss";
 import Swap from "../../Assets/swap.svg";
 import Sent from "../../Assets/sent.svg";
 import { STATUS } from '../../Constants';
-import { fixNumber } from "../../Helper/helper";
+import { fixNumber, numFormatter } from "../../Helper/helper";
 import { formatDate, shortner } from "../../Helper/helper"
 
 export default function HistoryItem({ historyItem, handleHistoryOpen }) {
@@ -22,9 +22,9 @@ export default function HistoryItem({ historyItem, handleHistoryOpen }) {
           </div>
 
         </div>
-        
+
         <div className={style.historySec__historyMarketSwap__rytSide}>
-          <h3>{historyItem?.amount ? fixNumber(historyItem.amount) : ""} 5ire</h3>
+          <h3>{historyItem?.amount ? numFormatter(fixNumber(historyItem.amount)) : ""} 5ire</h3>
           <p>
             Status : <span className={(historyItem.status.toLowerCase() === STATUS.PENDING.toLowerCase() || historyItem.status.toLowerCase() === STATUS.QUEUED.toLowerCase()) ? style.historySec__pending : (historyItem.status.toLowerCase() === STATUS.SUCCESS.toLowerCase() ? style.historySec__success : style.historySec__failed)}>{historyItem.status}</span>
           </p>
