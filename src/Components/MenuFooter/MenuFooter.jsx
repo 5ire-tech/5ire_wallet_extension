e1ccae772ec7252c631c4b85aa257d80b410225e
kulwinder.singh <kulwinder.singh@antiersolutions.com>
2023-05-05 13:30:51 +0530

    Fixed setting drawer

--- src/Components/MenuFooter/MenuFooter.jsx
+++ src/Components/MenuFooter/MenuFooter.jsx
@@ -78,8 +78,8 @@ function MenuFooter() {
             <Link
               to={ROUTES.HISTORY_P} // onClick={handleHistoryOpen}
               className={`${style.menuItems__items} ${pathname === ROUTES.HISTORY_P
-                  ? style.menuItems__items__active
-                  : ""
+                ? style.menuItems__items__active
+                : ""
                 }`}
             >
               <div className={style.menuItems__items__img}>
@@ -92,8 +92,8 @@ function MenuFooter() {
               to={ROUTES.MY_ACCOUNT}
               onClick={handleMyAccOpen}
               className={`${style.menuItems__items} ${pathname === ROUTES.MY_ACCOUNT
-                  ? style.menuItems__items__active
-                  : ""
+                ? style.menuItems__items__active
+                : ""
                 }`}
             >
               <div className={style.menuItems__items__img}>
@@ -105,8 +105,8 @@ function MenuFooter() {
             <Link
               onClick={() => setOpen2(true)}
               className={`${style.menuItems__items} ${pathname === ROUTES.MANAGE_WALLET
-                  ? style.menuItems__items__active
-                  : ""
+                ? style.menuItems__items__active
+                : ""
                 }`}
             >
               <div className={style.menuItems__items__img}>
@@ -165,7 +165,7 @@ function MenuFooter() {
         )}
       </Drawer> */}
 
-      {/* <Drawer
+      <Drawer
         height={404}
         title={
           <span style={{ display: "flex", alignItems: "center", gap: "8px" }}>
@@ -209,7 +209,7 @@ function MenuFooter() {
             </div>
           </div>
         </Link>
-        // {<Link to={ROUTES.PRIVACY_POLICY}> 
+        {/* <Link to={ROUTES.PRIVACY_POLICY}> */}
         <div
           className={style.sttings}
           style={{ marginTop: "14px" }}
@@ -242,10 +242,10 @@ function MenuFooter() {
             />
           </div>
         </div>
-    
+        {/* </Link> */}
 
         <SocialAccount />
-      </Drawer> */}
+      </Drawer>
 
       {pathname === ROUTES.BEFORE_BEGIN && <FooterStepOne />}
       {(pathname === ROUTES.NEW_WALLET_DETAILS) && <FooterStepTwo />}
