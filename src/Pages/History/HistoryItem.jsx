505e3ba703fd92fbcb6342a6a0dcae036ce01a1d
kulwinder.singh <kulwinder.singh@antiersolutions.com>
2023-05-23 12:55:42 +0530

    Added history icons

--- src/Pages/History/HistoryItem.jsx
+++ src/Pages/History/HistoryItem.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import style from "./style.module.scss";
-import Swap from "../../Assets/swap.svg";
-import Sent from "../../Assets/sent.svg";
+import yellow_send from "../../Assets/yellow_send.svg";
+import red_send from "../../Assets/red_send.svg";
+import green_send from "../../Assets/green_send.svg";
+import yellow_swap from "../../Assets/yellow_swap.svg";
+import red_swap from "../../Assets/red_swap.svg";
+import green_swap from "../../Assets/green_swap.svg";
+import yellow_contract from "../../Assets/yellow_contract.svg";
+import red_contract from "../../Assets/red_contract.svg";
+import green_contract from "../../Assets/green_contract.svg";
+
 import { isEqual } from '../../Utility/utility';
 import { formatDate } from "../../Helper/helper"
 import { fixNumber, numFormatter } from "../../Helper/helper";
 import { EVM, NATIVE, STATUS, TX_TYPE } from '../../Constants';
+
+
+function getTxIcon(type, status) {
+  switch (type) {
+    case TX_TYPE.SWAP:
+      return status === STATUS.SUCCESS.toLowerCase() ? green_swap : status === STATUS.FAILED.toLowerCase() ? red_swap : yellow_swap;
+    case TX_TYPE.CONTRACT_EXECUTION:
+    case TX_TYPE.CONTRACT_DEPLOYMENT:
+      return status === STATUS.SUCCESS.toLowerCase() ? green_contract : status === STATUS.FAILED.toLowerCase() ? red_contract : yellow_contract;
+    case TX_TYPE.SEND:
+    case TX_TYPE.NATIVE_APP:
+    case TX_TYPE.NATIVE_SIGNER:
+    default:
+      return status === STATUS.SUCCESS.toLowerCase() ? green_send : status === STATUS.FAILED.toLowerCase() ? red_send : yellow_send;
+
+  }
+}
 
 export default function HistoryItem({ historyItem, handleHistoryOpen }) {
   return (
@@ -15,7 +40,7 @@ export default function HistoryItem({ historyItem, handleHistoryOpen }) {
       </div>
       <div className={style.historySec__historyMarketSwap}>
         <div className={style.historySec__historyMarketSwap__leftSide}>
-          <img alt="hash" src={historyItem.intermidateHash ? Swap : Sent} />
+          <img alt="hash" src={getTxIcon(historyItem.type, historyItem.status.toLowerCase())} />
 
           <div className={style.historySec__historyMarketSwap__leftContact} onClick={() => { handleHistoryOpen(historyItem) }}>
             <h3>{isEqual(historyItem?.type, TX_TYPE.NATIVE_APP) ? historyItem?.method : historyItem.type}</h3>
