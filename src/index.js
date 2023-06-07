5cba880360e30d5468a7c76a1a8d721702bb281e
kulwinder.singh <kulwinder.singh@antiersolutions.com>
2023-06-07 10:50:53 +0530

    Commented close window code

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { EMTY_STR, LABELS } from "./Constants";
 import { getDataLocal } from "../src/Storage/loadstore";
 import { sessionStorage } from "../src/Storage/index";
 import { MessageOverStream } from "./Utility/message_helper";
-import Browser from "webextension-polyfill";
+// import Browser from "webextension-polyfill";
 
 // eslint-disable-next-line no-extend-native
 Number.prototype.noExponents = function () {
@@ -64,8 +64,8 @@ const initApp = (data, externalControlsState, windowAndTabState) => {
     const windowAndTabState = await getDataLocal(LABELS.WINDOW_AND_TAB_STATE);
 
     //handle the reopening the extension window using shortcut
-    const isPopup = Browser.extension.getViews({ type: "popup" });
-    !isPopup.length && !externalControlsState.activeSession && window.close();
+    // const isPopup = Browser.extension.getViews({ type: "popup" });
+    // !isPopup.length && !externalControlsState.activeSession && window.close();
 
     //created the transaction queue
     await getDataLocal(LABELS.TRANSACTION_QUEUE);
