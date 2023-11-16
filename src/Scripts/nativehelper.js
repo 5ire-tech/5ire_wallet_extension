7df8927e3398dded66d051766c10a9a59e2805ff
fullDev9233 <jimyy9211@gmail.com>
2023-11-15 22:19:16 -0700

    Update staking.bond func

--- src/Scripts/nativehelper.js
+++ src/Scripts/nativehelper.js
@@ -94,10 +94,10 @@ export default class ValidatorNominatorHandler {
 
     const bondedAmount = new BigNumber(stakeAmount).multipliedBy(DECIMALS).toFixed().toString();
 
-    const stashId = encodeAddress(nativeAddress);
-    const nominateTx = nativeApi.tx.staking.nominate(validatorsAccounts);
+    // const stashId = encodeAddress(nativeAddress);
+    const nominateTx = await nativeApi.tx.staking.nominate(validatorsAccounts);
     const points = await nativeApi.derive.staking?.currentPoints(); //find points
-    const bondOwnTx = await nativeApi.tx.staking.bond(stashId, bondedAmount, "Staked");
+    const bondOwnTx = await nativeApi.tx.staking.bond(bondedAmount, "Staked");
     const batchAll = await nativeApi.tx.utility.batchAll([bondOwnTx, nominateTx]);
 
     if (isFee) {
