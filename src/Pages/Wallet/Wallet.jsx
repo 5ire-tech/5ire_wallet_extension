cd044472bee7e7c2a4fa94e731df64d187561ac3
shweta saini <shweta@antiersolutions.com>
2024-07-02 12:16:43 +0530

    fix: favicon and logo changed and border added to input boxes
    Signed-off-by: shweta saini <shweta@antiersolutions.com>

--- src/Pages/Wallet/Wallet.jsx
+++ src/Pages/Wallet/Wallet.jsx
@@ -1,52 +1,57 @@
 import Send from "../Send/Send";
-import Swap from "../Swap/Swap.jsx";
+// import Swap from "../Swap/Swap.jsx";
 import style from "./style.module.scss";
-import { AuthContext } from "../../Store";
-import React, { useEffect, useState, useContext } from "react";
-import { Transfer } from "../../Assets/StoreAsset/StoreAsset";
+// import { AuthContext } from "../../Store";
+// import React, { useEffect,
+//    useState,
+//     useContext } from "react";
+// import { Transfer } from "../../Assets/StoreAsset/StoreAsset";
 
 function Wallet() {
-  const [activeTab, setActiveTab] = useState("send");
-  const { updateEstimatedGas } = useContext(AuthContext);
+  // const [activeTab, setActiveTab] = useState("send");
+  // const { updateEstimatedGas } = useContext(AuthContext);
 
-  useEffect(() => {
-    updateEstimatedGas(null);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [activeTab]);
+  // useEffect(() => {
+  //   updateEstimatedGas(null);
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [activeTab]);
 
-  const activeSend = () => {
-    setActiveTab("send");
-  };
+  // const activeSend = () => {
+  //   setActiveTab("send");
+  // };
   // const activeSwap = () => {
   //   setActiveTab("swap");
   // };
   return (
     <div className={style.wallet}>
       <div className={style.wallet__sendSwapSec}>
-        <div className={style.wallet__multiSwapBtn}>
-          <div className={style.wallet__sendSwapbtn}>
-            <button
+        {/* <div className={style.wallet__multiSwapBtn}> */}
+        {/* <div className={style.wallet__sendSwapbtn}> */}
+        {/* <button
               onClick={activeSend}
               className={`${style.firstButton} ${style.wallet__sendSwapbtn__buttons} 
               ${activeTab === "send" && style.wallet__sendSwapbtn__buttons__active}
             `}>
               <Transfer />
               Transfer
-            </button>
-            {/* <button
+            </button> */}
+        {/* <button
               onClick={activeSwap}
               className={`${style.secondtButton} ${style.wallet__sendSwapbtn__buttons}  ${
                 activeTab === "swap" && style.wallet__sendSwapbtn__buttons__active
               }`}>
               <Assets /> Assets
             </button> */}
-            {/* <div
+        {/* <div
               className={`${activeTab === "send" && style.activeFirst} ${
                 activeTab === "swap" && style.activeSecond
               } ${style.animations}`}></div> */}
-          </div>
+        {/* </div> */}
+        {/* </div> */}
+        <div className="innerScrool">
+          <Send />
         </div>
-        {activeTab === "send" && (
+        {/* {activeTab === "send" && (
           <div className="innerScrool">
             <Send />
           </div>
@@ -55,7 +60,7 @@ function Wallet() {
           <div className="innerScrool">
             <Swap />
           </div>
-        )}
+        )} */}
       </div>
     </div>
   );
